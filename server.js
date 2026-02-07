db60df96cf490c4e485e51bf36f180337fd3e759
Doge in trench coat <124916318+Cassius-K@users.noreply.github.com>
2026-02-06 21:53:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+var Express = require('express');
+var BodyParser = require('body-parser');
+var Server = express();
 
+Server.use(BodyParser.urlencoded({extended: false}));
+Server.use(BodyParser.json());
+
+Server.post("/", function(req, res){
+  console.log("I got "+req.body.message);
+  res.send(req.body.message);
+});
+
+Server.listen(3000, function(){
+  console.log("Express Echo Server is listening on port 3000");
